836907db61e9ab7006c7c0d6a698ae2a4740f630
Hung Nguyen <hungnguyen.130420@gmail.com>
2023-09-03 18:52:55 +0700

    build gradle

---
+++ src/test/java/com/app/TestClass.java
@@ -0,0 +1,10 @@
+package com.app;
+
+public class TestClass {
+    @Test
+    public void testMethod() throws Exception {
+        System.out.println("This is a test method.");
+
+        Test.main(null);
+    }
+}
